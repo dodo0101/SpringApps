81603b400ec8106540293caf96279f775c0ab8ba
Fedor_Glushchenko <f.glushenko@gmail.com>
2019-10-08 18:44:14 +0300

    First two lessons

---
+++ src/main/java/com/epam/spring/ClassicalMusic.java
@@ -0,0 +1,8 @@
+package com.epam.spring;
+
+public class ClassicalMusic implements Music {
+    @Override
+    public String getSong() {
+        return "Uvertura 1812";
+    }
+}
---
+++ src/main/java/com/epam/spring/RockMusic.java
@@ -0,0 +1,8 @@
+package com.epam.spring;
+
+public class RockMusic implements Music {
+    @Override
+    public String getSong() {
+        return "Smalls like teen spirit";
+    }
+}
